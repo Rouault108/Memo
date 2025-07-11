305a8f9f0c3ad7f20380cbd282b59980108bd7a1
Ruo Miyata <miyaty.ruo@gmail.com>
2025-07-11 17:10:23 +0900

    chore: ecdit next.config.ts

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '',
+  iamges: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
